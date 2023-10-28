0ceff29445580572f245f8f6cc2fe5af495617d5
Jedy Matt Tabasco <jedymatt@gmail.com>
2023-10-29 00:43:28 +0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,4 +2,11 @@ from setuptools import setup, find_packages
 from validation import __version__
 
 
-setup(name="validation-py", version=__version__, packages=find_packages(include=["validation", "validation.*"]))
+setup(
+    name="validation-py",
+    version=__version__,
+    packages=find_packages(
+        include=["validation", "validation.*"],
+    ),
+    long_description=open("README.md").read(),
+)
